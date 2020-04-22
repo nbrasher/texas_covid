7d13278575694f9a7c78556cfeb85fc2555eccc6
Nathan Brasher <nathan.brasher@bain.com>
2020-04-22 13:23:32 -0500

    fix: add server attribute

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ from views import all_counties_view
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+server = app.server
 
 # Calculate initial view for counties
 df, final_results = load_cases()
