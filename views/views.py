5a2dc0f8238234708041d899023bd428aa8a8b15
Nathan Brasher <nathan.brasher@bain.com>
2020-06-24 20:24:22 -0500

    fix: update new cases limit

--- views/views.py
+++ views/views.py
@@ -189,7 +189,7 @@ def county_detail_view(result, cases, area):
         font={'color': 'rgb(0,0,0)'},
         titlefont={'size': 12},
         yaxis1={'range': [.5, 1.5]}, 
-        yaxis2={'range': [0, 1500]},
+        yaxis2={'range': [0, 2000]},
         showlegend=False,
     )
     fig.update_xaxes(
